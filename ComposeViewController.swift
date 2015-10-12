ba55c716d7715baf1f6f92b407b3ca0f31f78014
Gilbert Guerrero <gilbert@gilbertguerrero.com>
2015-10-12 09:20:15 -0700

    Started on compose modal with partially transparent background

---
+++ ComposeViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ComposeViewController.swift
+//  TumblrDemo
+//
+//  Created by Gilbert Guerrero on 10/12/15.
+//  Copyright © 2015 Gilbert Guerrero. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onBackButton(sender: AnyObject) {
+
+        self.dismissViewControllerAnimated(true, completion: {})
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
